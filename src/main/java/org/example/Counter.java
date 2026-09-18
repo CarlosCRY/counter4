7ce6e674cb7b1f5720efe8d9f23b104112519272
CarlosCRY <trueccr@gmail.com>
2026-09-18 10:17:09 +0200

    Contador implementado

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -6,19 +6,24 @@ public class Counter implements Runnable {
     private final String name;
     private final int numVueltas;
 
-    public Counter (String name) {
+    public Counter (String name, int numVueltas) {
         this.name = name;
-    }
-
-    public Counter (int numVueltas) {
         this.numVueltas = numVueltas;
     }
 
     @Override
     public void run() {
         Random wait = new Random();
+        for (int i = 0; i < numVueltas; i++){
+            System.out.println("Contador " + name + " - Vuelta: " + i + 1);
+            try {
+                Thread.sleep(wait.nextInt(100,500));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
-        Thread.sleep(wait.nextInt(100,500));
+        }
+
 
     }
 }
